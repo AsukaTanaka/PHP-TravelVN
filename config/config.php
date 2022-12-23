abf7e101e74a1d270e3c85b0797bbb7c8c23b5ae
AsukaTanaka <111677080+AsukaTanaka@users.noreply.github.com>
2022-12-23 14:20:08 +0700

    Update

--- config/config.php
+++ config/config.php
@@ -1,5 +1,24 @@
 <?php 
 
+function openDB() {
+    
+    $host = "localhost";
+    $user = "";
+    $pass = "";
+    $db = "";
+    
+    $cn = mysqli_connect($host, $user, $pass, $db) or 
+        // die("Connect failed: %s". $cn);
+        die("Connect failed: ". mysqli_connect_error());
 
+    return $cn;
+}
+
+function closeDB($cn) {
+
+    mysqli_close($cn);
+    // $cn->close();
+
+}
 
 ?>
